d65d0f87c81517ce0b45c9f7dffa7fb1bbdc0e9b
Farzan-I <farzan_15@hotmail.co.uk>
2022-06-20 22:12:34 +0100

    Correct errors using Rubocop

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,29 +1,31 @@
-class Bank
+# frozen_string_literal: false
 
+# Passing all tests
+class Bank
   def initialize
-    @header = "date   || credit || debit || balance"
+    @header = 'date   || credit || debit || balance'
     @balance = 0
     @amount_moved = 0
     @messages = []
   end
 
   def statement
-    self.calc_statement.reverse
+    calc_statement.reverse
   end
 
   def deposit(amount, date)
-    self.calc_deposit(amount, date)
+    calc_deposit(amount, date)
   end
 
   def withdraw(amount, date)
-    self.calculate_withdraw(amount, date)
+    calculate_withdraw(amount, date)
   end
 
   private
 
   def calc_statement
     @messages.push(@header)
-    full_statement = @messages.each do |transaction|
+    @messages.each do |transaction|
       puts transaction
     end
   end
@@ -31,17 +33,16 @@ class Bank
   def calc_deposit(amount, date)
     @balance += amount
     @amount_moved = amount
-    @messages.push "#{date} || %.2f" % [@amount_moved] + " || || %.2f" % [@balance]
+    @messages.push "#{date} || %.2f" % [@amount_moved] + ' || || %.2f' % [@balance]
   end
 
   def calculate_withdraw(amount, date)
     if amount > @balance
-      "Exceeded available balance, please withdraw a smaller amount"
+      'Exceeded available balance, please withdraw a smaller amount'
     else
       @balance -= amount
       @amount_moved = amount
-      @messages.push "#{date} || || %.2f" % [@amount_moved] + " || %.2f" % [@balance]
+      @messages.push "#{date} || || %.2f" % [@amount_moved] + ' || %.2f' % [@balance]
     end
   end
-  
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,48 +1,41 @@
+# frozen_string_literal: false
+
 require 'bank_account'
 
 describe Bank do
-
   before(:each) do
     @bank = Bank.new
   end
-  
   context 'header of statement' do
     it 'displays the heading of the statement' do
-      expect(@bank.statement).to eq ["date   || credit || debit || balance"]
+      expect(@bank.statement).to eq ['date   || credit || debit || balance']
     end
   end
-
   context 'deposit can be made' do
     it 'deposits £50, statement updates in credit and balance on 20/06/2022' do
       @bank.deposit(50, '20/06/2022')
-
-      expect(@bank.statement).to eq ["date   || credit || debit || balance", 
-        "20/06/2022 || 50.00 || || 50.00"]
+      expect(@bank.statement).to eq ['date   || credit || debit || balance',
+        '20/06/2022 || 50.00 || || 50.00']
     end
   end
-
   context 'withdraw can be made' do
     it 'shows withdrawal of £25.00 on 22/06/2022 from £50.00 balance' do
       @bank.deposit(50, '20/06/2022')
       @bank.withdraw(25, '22/06/2022')
-    
-      expect(@bank.statement).to eq ["date   || credit || debit || balance", 
-        "22/06/2022 || || 25.00 || 25.00", 
-        "20/06/2022 || 50.00 || || 50.00"]
+      expect(@bank.statement).to eq ['date   || credit || debit || balance',
+        '22/06/2022 || || 25.00 || 25.00',
+        '20/06/2022 || 50.00 || || 50.00']
     end
   end
-
   context 'original test criteria' do
     it 'tests the original criteria of the bank tech test' do
       @bank.deposit(1000, '10/01/2023')
       @bank.deposit(2000, '13/01/2023')
       @bank.withdraw(500, '14/01/2023')
-      
-      expect(@bank.statement).to eq (['date   || credit || debit || balance',
+      expect(@bank.statement).to eq ['date   || credit || debit || balance',
         '14/01/2023 || || 500.00 || 2500.00',
         '13/01/2023 || 2000.00 || || 3000.00',
-        '10/01/2023 || 1000.00 || || 1000.00'])
+        '10/01/2023 || 1000.00 || || 1000.00']
     end
   end
-
 end
