cc9b77fe66ecd8166e2606dc162780998f0daa78
Farzan-I <farzan_15@hotmail.co.uk>
2022-06-20 17:07:26 +0100

    second commit first failing test

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,3 @@
 class Bank
-
+  
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,11 @@
 require 'bank_account'
 
 describe Bank do
-  
+  before(:each) do
+    @bank = Bank.new
+  end
+
+  it 'displays the heading of the statement' do
+    expect(@bank.statement).to eq "date   || credit || debit || balance"
+  end
 end
