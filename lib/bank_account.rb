862010a04a658ea67a0a8fe9d026712e10d496af
Farzan-I <farzan_15@hotmail.co.uk>
2022-06-20 20:28:10 +0100

    Implement private methods to pass second test

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,32 @@
 class Bank
+
+  def initialize
+    @header = "date   || credit || debit || balance"
+    @balance = 0
+    @amount_moved = 0
+    @messages = []
+  end
+
   def statement
-    "date   || credit || debit || balance"
+    self.calc_statement.reverse
+  end
+
+  def deposit(amount, date)
+    self.calc_deposit(amount, date)
+  end
+
+  private
+
+  def calc_deposit(amount, date)
+    @balance += amount
+    @amount_moved = amount
+    @messages.push "#{date} || %.2f" % [@amount_moved] + " || || %.2f" % [@balance]
+  end
+
+  def calc_statement
+    @messages.push(@header)
+    full_statement = @messages.each do |transaction|
+      puts transaction
+    end
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,6 +6,14 @@ describe Bank do
   end
 
   it 'displays the heading of the statement' do
-    expect(@bank.statement).to eq "date   || credit || debit || balance"
+    expect(@bank.statement).to eq ["date   || credit || debit || balance"]
   end
+
+  it 'deposits £50, statement updates in credit and balance on 20/06/2022' do
+    @bank.deposit(50, '20/06/2022')
+
+    expect(@bank.statement).to eq ["date   || credit || debit || balance", 
+      "20/06/2022 || 50.00 || || 50.00"]
+  end
+
 end
