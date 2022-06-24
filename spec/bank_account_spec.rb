16a3e78afad6f24d4b120ba76ab3575635c8b959
Farzan-I <farzan_15@hotmail.co.uk>
2022-06-24 17:31:59 +0100

    moved onto account tests, pended tests and started from scratch with first failing test

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,22 +4,27 @@ require 'bank_account'
 
 describe Bank_account do
   before(:each) do
-    @bank = Bank_account.new
+    @account = Bank_account.new
   end
+
+  it 'starts with an empty balance (0)' do
+    expect(@account.balance).to eq 0
+  end
+
   context 'header of statement' do
-    it 'displays the heading of the statement' do
+    xit 'displays the heading of the statement' do
       expect(@bank.statement).to eq ['date   || credit || debit || balance']
     end
   end
   context 'deposit can be made' do
-    it 'deposits £50, statement updates in credit and balance on 20/06/2022' do
+    xit 'deposits £50, statement updates in credit and balance on 20/06/2022' do
       @bank.deposit(50, '20/06/2022')
       expect(@bank.statement).to eq ['date   || credit || debit || balance',
         '20/06/2022 || 50.00 || || 50.00']
     end
   end
   context 'withdraw can be made' do
-    it 'shows withdrawal of £25.00 on 22/06/2022 from £50.00 balance' do
+    xit 'shows withdrawal of £25.00 on 22/06/2022 from £50.00 balance' do
       @bank.deposit(50, '20/06/2022')
       @bank.withdraw(25, '22/06/2022')
       expect(@bank.statement).to eq ['date   || credit || debit || balance',
@@ -28,7 +33,7 @@ describe Bank_account do
     end
   end
   context 'original test criteria' do
-    it 'tests the original criteria of the bank tech test' do
+    xit 'tests the original criteria of the bank tech test' do
       @bank.deposit(1000, '10/01/2023')
       @bank.deposit(2000, '13/01/2023')
       @bank.withdraw(500, '14/01/2023')
