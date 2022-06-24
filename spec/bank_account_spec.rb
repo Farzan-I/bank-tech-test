dc53b1d229c8ed3d6609964d4717708a83d71457
Farzan-I <farzan_15@hotmail.co.uk>
2022-06-24 18:00:42 +0100

    refactor test for account within deposit describe block

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -9,34 +9,22 @@ describe Bank_account do
   let(:transaction_class_dbl) { double :transaction_class, new: mock_transaction }
   let(:account) { Bank_account.new(statement_class = statement_class_dbl, transaction_class = transaction_class_dbl)}
 
-  # before(:each) do
-  #   @account = Bank_account.new
-  # end
-
   it 'starts with an empty balance (0)' do
     expect(account.balance).to eq 0
   end
 
-  it 'allows client to deposit an amount into their account' do
-    account.deposit(1000)
-    expect(account.balance).to eq 1000
+  describe '#deposit' do
+    it 'allows client to deposit an amount into their account' do
+      account.deposit(1000)
+      expect(account.balance).to eq 1000
+    end
+
+    it 'calls the statement class to add the specified transaction' do
+      expect(mock_statement).to receive(:add)
+      expect(transaction_class_dbl).to receive(:new)
+      account.deposit(1000)
+    end
   end
-
-  it 'calls the statement class to add the specified transaction' do
-    expect(mock_statement).to receive(:add)
-    expect(transaction_class_dbl).to receive(:new)
-    account.deposit(1000)
-  end
-
-  # context 'deposit can be made' do
-  #   xit 'deposits £50, statement updates in credit and balance on 20/06/2022' do
-  #     @bank.deposit(50, '20/06/2022')
-  #     expect(@bank.statement).to eq ['date   || credit || debit || balance',
-  #       '20/06/2022 || 50.00 || || 50.00']
-  #   end
-  # end
-
-
 
   context 'withdraw can be made' do
     xit 'shows withdrawal of £25.00 on 22/06/2022 from £50.00 balance' do
@@ -47,6 +35,7 @@ describe Bank_account do
         '20/06/2022 || 50.00 || || 50.00']
     end
   end
+
   context 'original test criteria' do
     xit 'tests the original criteria of the bank tech test' do
       @bank.deposit(1000, '10/01/2023')
